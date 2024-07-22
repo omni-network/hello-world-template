53f72bbaf0b17e415eed2fa54362abf8febf33ac
ttarsi <31632172+ttarsi@users.noreply.github.com>
2024-07-22 08:48:18 -0400

    fix greeting structure and receipt (#6)

--- src/GlobalGreeter.sol
+++ src/GlobalGreeter.sol
@@ -21,7 +21,7 @@ contract GlobalGreeter is XApp {
      * @notice The latest greeting recorded by the contract
      * @dev State variable to store information about the latest greeting received by the contract
      */
-    Greeting public lastGreet = Greeting(0, 0, 0, address(0), address(0), "Hello Omni!");
+    Greeting public lastGreet = Greeting(0, 0, address(0), "Hello Omni!");
 
     /**
      * @dev Initializes a new GlobalGreeter contract with the specified portal address
@@ -36,16 +36,9 @@ contract GlobalGreeter is XApp {
      *      It updates the lastGreet variable with information about the received greeting.
      */
     function greet(string calldata _greeting) external xrecv {
-        // Initialize the fee to 0, for local calls
-        uint256 fee = 0;
-        if (isXCall() && xmsg.sourceChainId != omni.chainId()) {
-            // Calculate the fee for the cross-chain call
-            fee = feeFor(xmsg.sourceChainId, abi.encodeWithSelector(this.greet.selector, _greeting), DEST_TX_GAS_LIMIT);
-        }
-
         // Create a Greeting struct to store information about the received greeting
         Greeting memory greeting =
-            Greeting(xmsg.sourceChainId, block.timestamp, fee, msg.sender, xmsg.sender, _greeting);
+            Greeting(xmsg.sourceChainId, block.timestamp, xmsg.sender, _greeting);
 
         // Update the lastGreet variable with the information about the received greeting
         lastGreet = greeting;
