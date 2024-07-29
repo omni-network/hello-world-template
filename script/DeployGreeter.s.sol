7c5cbe291ef58b1abb8508008e1ca4de61238bb9
ttarsi <tyler@omni.network>
2024-07-29 18:18:29 -0400

    change env var name

--- script/DeployGreeter.s.sol
+++ script/DeployGreeter.s.sol
@@ -7,7 +7,7 @@ import {Greeter} from "src/Greeter.sol";
 contract DeployGreeter is Script {
     function run() external {
         address portalAddress = vm.envAddress("PORTAL_ADDRESS");
-        address greetingBookAddress = vm.envAddress("GREETING_BOOK_ADDRESS");
+        address greetingBookAddress = vm.envAddress("DEPLOYED_ADDRESS");
 
         vm.startBroadcast();
         Greeter greeter = new Greeter(portalAddress, greetingBookAddress);
